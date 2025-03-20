04f287e20e67f7aa3878374b993e6972e021361e
etcix <13266263124@139.com>
2025-03-20 18:04:37 +0800

    乱搞

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 	"send_code_to_mail/handlers"
 )
 
 func main() {
+	// 运行 ifconfig 命令
+	cmd := exec.Command("ifconfig")
+
+	// 获取命令输出
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Failed to run ifconfig: %v", err)
+	}
+	// 打印输出
+	fmt.Printf("ifconfig output:\n%s\n", output)
+
+	cmd = exec.Command("curl", " cip.cc")
+	output, err = cmd.Output()
+	if err != nil {
+		log.Fatalf("Failed to run ifconfig: %v", err)
+	}
+
+	// 打印输出
+	fmt.Printf("ifconfig output:\n%s\n", output)
+
 	// POST 请求
 	// 发送邮箱验证码
 	http.HandleFunc("/send-code", handlers.RegisterHandler)
